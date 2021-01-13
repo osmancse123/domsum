f81a65b5d5b038eb7aeb34d73d08d5597b836c43
osman <osmangoni2k16@gmail.com>
2021-01-14 01:29:18 +0600

    traversing dom hierarchy

--- script2.js
+++ script2.js
@@ -1,2 +1,19 @@
-// Traversing DOM element - play with DOM:-
+// // Traversing DOM element - play with DOM:-
 
+// // Get Element By Id:
+// // let element = document.getElementById('head');
+// // console.log(element);
+
+// let haderElement = document.getElementById('head');
+// console.log(haderElement.textContent);
+// console.log(haderElement.innerText);
+// console.log(haderElement.innerHTML);
+
+// let headElement = document.getElementById('head');
+// headElement.style.fontSize = '70px';
+// headElement.style.color = 'red';
+
+// console.dir(document);
+
+// Get Element By Class:
+// console.log(document.getElementsByClassName('content'));
